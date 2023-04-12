dcdbe35105c19a84011a1167564a94ba55f51d7a
green1490 <zambokrisztian1490@gmail.com>
2023-04-12 20:40:31 +0200

    change mistake

--- src/index.js
+++ src/index.js
@@ -14,6 +14,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -25,12 +27,17 @@ app.listen(port, () => {
 app.get('/login/name/:name/password/:password', (req,res) => {
   connection.query(`select * from users where UserPassword = '${req.params['password']}' `, (err,result,fields) => {
     if (result.length != 0) {
-      res.send('Siker')
+      res.send(result)
     } else {
       res.send('Rossz jelszó')
     }
   })
 })
 
-
-
+app.get('/mistakeUpdate/userID/ID/:id/mistakes/:mistakes', (req,res) => {
+  connection.query(`update users set UserMistakes='${req.params['mistakes']}' where UserID = '${req.params['UserID']}'`, (err,result,fields) => {
+    if (err) {
+      res.send(err)
+    }
+  })
+})
