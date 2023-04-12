a66d85c5ca53722b34de8b75336232ad801a5770
green1490 <zambokrisztian1490@gmail.com>
2023-04-12 18:31:29 +0200

    implement login

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 //dipdb1
 // create the connection to database
+//login
+
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -14,13 +16,20 @@ const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-  connection.query("select 1" , (err, results, fields) => {
-    console.log(results);
-  })
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+app.get('/login/name/:name/password/:password', (req,res) => {
+  connection.query(`select * from users where UserPassword = '${req.params['password']}' `, (err,result,fields) => {
+    if (result.length != 0) {
+      res.send('Siker')
+    } else {
+      res.send('Rossz jelszó')
+    }
+  })
 })
 
 
